24b91b6155eabf0ff9be3eeaa9f315dd168c3a34
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2024-05-21 12:49:50 +0000

    Added Drawer in Stories page to view single story

--- Namaste/Client/src/components/StoriesCard.jsx
+++ Namaste/Client/src/components/StoriesCard.jsx
@@ -4,13 +4,13 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 
-export default function StoriesCard(props) {
+export default function StoriesCard({ img, onClick }) {
   return (
-    <Card sx={{ width: 272, height:375, borderRadius:2 }}>
+    <Card sx={{ width: 272, height:375, borderRadius:2 }} onClick={onClick}>
       <CardActionArea>
         <CardMedia
           component="img"
-          image={props.img}
+          image={img}
           alt="green iguana"
           sx={{backgroundColor:"blue", height:375}}
         />
